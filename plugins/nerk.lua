c6bf4f4ce3812a2b27562d32ee7708696f1bfeaa
avatartg <avatarbot20@gmail.com>
2016-07-19 15:51:34 +0430

    Create nerk.lua

---
+++ plugins/nerk.lua
@@ -0,0 +1,46 @@
+do
+
+function run(msg, matches)
+  return [[ 
+groupNerkh groupNerkh 💥تَــــــعــــــږفـــــہ جڍیڍہ avata_tg💥
+♻️♻️♻️♻️♻️♻️♻️♻️♻️♻️♻️♻️♻️♻️♻️
+🔺نږخہ رباټ ضد لینڪ و ضد اتڪ برایہ سوپږ ڴږۅہ🔻🔻🔻
+1⃣ماهــــــــــیانـــــہ⬅️30,000 ریال 
+2⃣ڍۅ مــــــــــاه⬅️⬅️50,000 ریال 
+🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚🔚
+🔵خرید ږباټ بڍۅنــــــہ تاږیخہ انقضا🔴⬇️
+70,000ریال 
+☑️☑️☑️☑️☑️☑️☑️☑️☑️☑️☑️🔚
+🔴بږایہ خږیڍ و سفاږشاټ بہ ایڹ یۅزږنِیم مږاجعہ ڪنیڍ⬇️⬇️⬇️⬇️
+🔥@Ali_dev_AVATAR🔥
+
+@avatar_sudo
+🐾🐾🐾🐾🐾🐾🐾🐾🐾🐾🐾🐾
+دږ صۅږت ریپۅږت بودن به اين ږبات پیام بدید⬇️
+🔥@SUDO_Ali_bot🔥 ➖🎗➖➖➖➖➖➖➖➖➖➖➖➖➖🎗
+🌐Our Channel🌐✅⬇️
+@AVATAR_TG_TM
+🔶🔶🔶🔶🔶🔶🔶🔶🔶🔶🔶🔶
+🔧لینڪہ گږوهہ پشتیبانے⚙⬇️
+https://telegram.me/joinchat/DKdhIz-XHa8u08f3ZZrZTA
+]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "ver: Shows bot version",
+  patterns = {
+    "^[Nn]erk$",
+    "^[Nn]erk$",
+    "^!nerk$",
+    "^%nerk$",
+    "^$nerkbot$",
+   "^#nerk$",
+   "^#nerk$",
+   "^/nerk$",
+   "^#nerk$",
+  }, 
+  run = run 
+}
+
+end
