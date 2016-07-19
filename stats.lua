8b212c444c486ae00308cf61fd0dc2cf79da8a6e
avatartg <avatarbot20@gmail.com>
2016-07-19 11:16:32 +0430

    Update and rename plugins/smartstats.lua to stats.lua

--- stats.lua
+++ stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'smart' then -- Put everything you like :)
+  if matches[1]:lower() == 'avatar' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used smart ")
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "smart" then -- Put everything you like :)
+    if matches[2] == "avatar" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "تنها برای ادمین هاست !"
       else
@@ -143,8 +143,8 @@ return {
     "^([Ss]tats)$",
     "^([Ss]tatslist)$",
     "^([Ss]tats) (group) (%d+)",
-    "^([Ss]tats) (smart)",
-	"^([Ss]mart)"
+    "^([Ss]tats) (avatar)",
+	"^([Aa]vatar)"
     }, 
   run = run
 }
